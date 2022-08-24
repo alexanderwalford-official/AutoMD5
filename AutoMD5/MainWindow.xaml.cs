3d7311c7e3431881587eaffbb35e72d3da735be7
Alexander Walford <74402359+alexanderwalford-official@users.noreply.github.com>
2022-08-24 22:10:33 +0100

    Base fucntionality. Pleanty of bugs that need fixing!

--- AutoMD5/MainWindow.xaml.cs
+++ AutoMD5/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,28 +25,158 @@ namespace AutoMD5
         public MainWindow()
         {
             InitializeComponent();
+            getchecks();
         }
 
         void getchecks()
         {
-            if (File.Exists(datafile))
+            try
             {
-                // cont
-                string[] files = Directory.GetFiles(@"c:\\ProgramData\AutoMD5\files\");
-
-                foreach (string file in files)
+                if (File.Exists(datafile))
                 {
-                    filelist.Items.Add(file);
-                }
+                    // cont
+                    string s = File.ReadAllText(datafile);
+                    string[] lines = s.Split('[');
+                    filelist.Items.Clear();
 
+                    foreach (string f in lines)
+                    {
+                        string s1 = f.Replace("[", "").Replace("]", ""); // m:###################|i:instructor.bat|f:file.file|auto_download:1|auto_exec:1
+                        string[] s1s = s.Split('|'); // split each propery into it an array
+
+                        filelist.Items.Add(s1s[2].Replace("f:", "")); // add file name
+                    }
+
+                }
+                else
+                {
+                    // create the required files
+                    Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\");
+                    Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\files\");
+                    File.Create(datafile);
+                }
             }
-            else
+            catch
             {
-                // create the required files
-                Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\");
-                Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\files\");
-                File.Create(datafile);
             }
+
+        }
+
+        void getdata(string input)
+        {
+            try
+            {
+                string s = File.ReadAllText(datafile);               
+                string[] lines = s.Split('[');
+
+                foreach (string f in lines)
+                {
+                    if (f.Contains(input))
+                    {
+                        string s1 = f.Replace("[", "").Replace("]", ""); // m:###################|i:instructor.bat|f:file.file|auto_download:1|auto_exec:1
+                        string[] s1s = s.Split('|'); // split each propery into it an array
+
+                        title.Content = s1s[2].Replace("f:", ""); // set the title to the file's name
+                        file_md5value.Content = s1s[0].Replace("[m:", ""); // set the md5 title
+                        string instructorfilecontents = "NO DEFINED INSTRUCTIONS FILE";
+                        try
+                        {
+                            instructorfilecontents = s1s[1].Replace("i:", "") + " (INSTRUCTOR FILE):\n" + File.ReadAllText(s1s[1].Replace("i:", ""));
+                        }
+                        catch
+                        {
+                            // no instructor file
+                        }
+                        instructortext.Text = s1s[2].Replace("f:", "") + ": \n" + s1s[3] + "\n" + s1s[4].Replace("]","") + "\n\n" + instructorfilecontents;
+
+                        // re-download and calculate new hash
+                        // add back removed characters
+                        string s1_url = s1s[2].Replace("f:", "");
+                        string url;
+
+                        if (s1_url.StartsWith("s"))
+                        {
+                            // ssl mode
+                            s1_url = s1_url.Remove(0, 1); // remove s so we can add it back to the correct part
+                            url = s1_url = "https:" + s1_url;
+                        }
+                        else
+                        {
+                            url = s1_url = "http:" + s1_url;
+                        }
+                        
+                        string url_final = url.Replace("_fs_", "/");
+                        Console.WriteLine(url_final);
+
+                        string newmd5 = "";
+                        string filename = s1s[2].Replace("f:", "");
+
+                        try
+                        {
+                            // create a temp working dir
+                            Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\tmp\");
+
+                            // download the file to a temp dir
+                            using (var client = new WebClient())
+                            {
+                                client.DownloadFile(url_final, @"c:\\ProgramData\AutoMD5\tmp\" + filename);
+                            }
+
+                            // check the md5
+                            using (var md5 = MD5.Create())
+                            {
+                                using (var stream = File.OpenRead(@"c:\\ProgramData\AutoMD5\tmp\" + filename))
+                                {
+                                    var hash = md5.ComputeHash(stream);
+                                    newmd5 = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                                }
+                            }
+                            string oldmd5 = "";
+                            file_md5value.Content = "";
+                            oldmd5 = file_md5value.Content.ToString();
+                            // compare md5s
+                            if (newmd5 != oldmd5)
+                            {
+                                // file is outdated! move new file to files folder and update md5 value
+                                statustext.Content = "Outdated MD5.";
+
+                                // copy the file out of temp into files folder
+                                File.Copy(@"c:\\ProgramData\AutoMD5\tmp\" + filename, @"c:\\ProgramData\AutoMD5\files\" + filename);
+
+                                // update only md5 in file
+                                string data = File.ReadAllText(datafile).Replace(oldmd5, newmd5);
+                                File.WriteAllText(datafile, data);
+
+                                statustext.Content = "File updated just now.";
+                            }
+                            else
+                            {
+                                statustext.Content = "File is up to date.";
+                            }
+                            try
+                            {
+                                // execute instructor
+                                System.Diagnostics.Process.Start(s1s[1].Replace("i:", ""));
+                            }
+                            catch
+                            {
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                title.Content = "ERROR READING ENTRY";
+                file_md5value.Content = "?????????";
+                instructortext.Text = e.Message;
+            }
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -69,7 +201,18 @@ namespace AutoMD5
 
         private void filelist_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            object o = null;
+            try
+            {
+                // menu item clicked
+                o = filelist.SelectedItem;
 
+                if (o != null)
+                {
+                    getdata(o.ToString());
+                }  
+            }
+            catch { }
         }
     }
 }
--- AutoMD5/NewFile.xaml.cs
+++ AutoMD5/NewFile.xaml.cs
@@ -22,6 +22,8 @@ namespace AutoMD5
     {
         string datafile = @"c:\\ProgramData\AutoMD5\files.txt";
         string newinstructorfile = "";
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        bool hasinstructor = false;
 
         public NewFile()
         {
@@ -30,61 +32,127 @@ namespace AutoMD5
 
         public void DownloadFile()
         {
-            // first, we need to add the record to the files list file
-            File.WriteAllText(datafile, File.ReadAllText(datafile) + "\n[m:" + md5previewtext.Content + "|i:" + newinstructorfile + "|f:" + linkinputbox.Text + "|auto_download:" + AutoUpdateCheck + "|auto_exec:" + autoexeccheck + "]");
-
-            // now download the file to /files
             try
             {
+                // add an exclusive link paramter for rechecking?
+
+                string filename = linkinputbox.Text.Replace("/", "_fs_").Replace("http", "").Replace(":", ""); // clean the string
+                // first, we need to add the record to the files list file
+                File.WriteAllText(datafile, File.ReadAllText(datafile) + "\n[m:" + md5previewtext.Content + "|i:" + newinstructorfile + "|f:" + filename + "|auto_download:" + AutoUpdateCheck.IsChecked + "|auto_exec:" + autoexeccheck.IsChecked + "]");
+
+                // now download the file to /files
+
                 using (var client = new WebClient())
                 {
-                    client.DownloadFile(linkinputbox.Text, @"c:\\ProgramData\AutoMD5\files\" + linkinputbox.Text);
+                    client.DownloadFile(linkinputbox.Text, @"c:\\ProgramData\AutoMD5\files\" + filename);
                 }
+
+                if (hasinstructor)
+                {
+                    // try to execute the instructor batch file
+                    System.Diagnostics.Process.Start(openFileDialog.FileName);
+                }
+
+                // close window, it worked!
+                this.Close();
             }
             catch
             {
+                if (hasinstructor)
+                {
+                    warntext.Content = "ERROR: Could not add file. Check instructor.";
+                }
+                else
+                {
+                    warntext.Content = "ERROR: Issue with remote file.";
+                }
                 // could not download file
-
             }
         }
 
         private void addbutton_Copy_Click(object sender, RoutedEventArgs e)
         {
             // select instructor file
-            OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Batch files (*.bat)|*.bat";
             if (openFileDialog.ShowDialog() == true)
             {
                 instrcutorfiletext.Content = openFileDialog.FileName;
                 newinstructorfile = openFileDialog.FileName;
+                hasinstructor = true;
             }
         }
 
         private void link_TextChanged(object sender, TextChangedEventArgs e)
         {
+        }
+
+        public void checkcompat ()
+        {
+            string filename = linkinputbox.Text.Replace("/", "_fs_").Replace("http", "").Replace(":", ""); // clean the string
             try
             {
+                checkremote.IsEnabled = false;
+                // check if relevent files have been created
+                if (!File.Exists(datafile))
+                {
+                    // create the required files
+                    Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\");
+                    Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\files\");
+                    File.Create(datafile);
+                }
+
+                // create a temp working dir
+                Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\tmp\");
+                // download the file to a temp dir
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(linkinputbox.Text, @"c:\\ProgramData\AutoMD5\tmp\" + filename);        
+                }
+
+                // check the md5
                 using (var md5 = MD5.Create())
                 {
-                    using (var stream = File.OpenRead(linkinputbox.Text))
+                    using (var stream = File.OpenRead(@"c:\\ProgramData\AutoMD5\tmp\" + filename))
                     {
-                        md5previewtext.Content = stream;
+                        var hash = md5.ComputeHash(stream);
+                        md5previewtext.Content = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
                     }
                 }
+
+                // enable the add button
                 addbutton.IsEnabled = true;
             }
-            catch
+            catch (Exception e)
             {
                 md5previewtext.Content = "ERROR: INVALID FILE URL PROVIDED.";
                 addbutton.IsEnabled = false;
+                Console.WriteLine(e.Message);
             }
 
+            // clean up the temp folder
+            if (Directory.Exists(@"c:\\ProgramData\AutoMD5\tmp\"))
+            {
+                try
+                {
+                    File.Delete(@"c:\\ProgramData\AutoMD5\tmp\" + filename);
+                    Directory.Delete(@"c:\\ProgramData\AutoMD5\tmp\");
+                }
+                catch
+                {
+                }
+            }
+            checkremote.IsEnabled = true;
         }
 
         private void addbutton_Click(object sender, RoutedEventArgs e)
         {
             // add file
             DownloadFile();
+        }
+
+        private void checkremote_Click(object sender, RoutedEventArgs e)
+        {
+            checkcompat();
         }
     }
 }
