72d5ad75f5973f360939ac9f1d6a40498e102eb8
Alexander Walford <74402359+alexanderwalford-official@users.noreply.github.com>
2022-08-24 20:02:57 +0100

    Basic backend complete.

--- AutoMD5/MainWindow.xaml.cs
+++ AutoMD5/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,58 @@ namespace AutoMD5
 {
     public partial class MainWindow : Window
     {
+        string datafile = @"c:\\ProgramData\AutoMD5\files.txt";
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        void getchecks()
+        {
+            if (File.Exists(datafile))
+            {
+                // cont
+                string[] files = Directory.GetFiles(@"c:\\ProgramData\AutoMD5\files\");
 
+                foreach (string file in files)
+                {
+                    filelist.Items.Add(file);
+                }
 
+            }
+            else
+            {
+                // create the required files
+                Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\");
+                Directory.CreateDirectory(@"c:\\ProgramData\AutoMD5\files\");
+                File.Create(datafile);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // new file
+            var win = new NewFile();
+            win.Show();
+        }
+
+        private void checkupdates_Click(object sender, RoutedEventArgs e)
+        {
+            // recheck the MD5s
+            getchecks();
+        }
+
+        private void aboutbutton_Click(object sender, RoutedEventArgs e)
+        {
+            // credits
+            var win = new info();
+            win.Show();
+        }
+
+        private void filelist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
--- AutoMD5/NewFile.xaml.cs
+++ AutoMD5/NewFile.xaml.cs
@@ -11,14 +11,18 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.IO;
+using Microsoft.Win32;
+using System.Net;
 
 namespace AutoMD5
 {
-    /// <summary>
-    /// Interaction logic for NewFile.xaml
-    /// </summary>
     public partial class NewFile : Window
     {
+        string datafile = @"c:\\ProgramData\AutoMD5\files.txt";
+        string newinstructorfile = "";
+
         public NewFile()
         {
             InitializeComponent();
@@ -26,7 +30,61 @@ namespace AutoMD5
 
         public void DownloadFile()
         {
+            // first, we need to add the record to the files list file
+            File.WriteAllText(datafile, File.ReadAllText(datafile) + "\n[m:" + md5previewtext.Content + "|i:" + newinstructorfile + "|f:" + linkinputbox.Text + "|auto_download:" + AutoUpdateCheck + "|auto_exec:" + autoexeccheck + "]");
 
+            // now download the file to /files
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(linkinputbox.Text, @"c:\\ProgramData\AutoMD5\files\" + linkinputbox.Text);
+                }
+            }
+            catch
+            {
+                // could not download file
+
+            }
+        }
+
+        private void addbutton_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            // select instructor file
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Batch files (*.bat)|*.bat";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                instrcutorfiletext.Content = openFileDialog.FileName;
+                newinstructorfile = openFileDialog.FileName;
+            }
+        }
+
+        private void link_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(linkinputbox.Text))
+                    {
+                        md5previewtext.Content = stream;
+                    }
+                }
+                addbutton.IsEnabled = true;
+            }
+            catch
+            {
+                md5previewtext.Content = "ERROR: INVALID FILE URL PROVIDED.";
+                addbutton.IsEnabled = false;
+            }
+
+        }
+
+        private void addbutton_Click(object sender, RoutedEventArgs e)
+        {
+            // add file
+            DownloadFile();
         }
     }
 }
